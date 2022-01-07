412c28a67d1791d505606ed22e7e984222dc4828
joelapoi <55506069+joelapoi@users.noreply.github.com>
2022-01-07 16:00:44 +0700

    setting routes di serverjs

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
+
+//panggil routes
+var routes = require('./routes')
+routes(app);
+
 app.listen(3000, () => {
   console.log('server started on port');
 });
